aa9dfd436b698007db041fd1616291a9668376bc
Maclairen Rogers <Mac.Elise2020@gmail.com>
2025-08-15 12:58:28 -0400

    initialized E-commerce repository

--- books.js
+++ books.js
@@ -1,87 +1,88 @@
 let books; 
- async function renderBooks(filter) {
-    const booksWrapper = document.querySelector('.books'); 
 
-    booksWrapper.classList += ' books__loading'
+async function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books"); 
 
-if (!books) {
+  booksWrapper.classList += ' books__loading'
+
+  if (!books) {
     books = await getBooks(); 
-}
+  }
 
-booksWrapper.classList.remove('books__loading')
+  booksWrapper.classList.remove('books__loading')
 
-    if (filter === 'LOW_TO_HIGH') { 
-        books.sort(
-          (a, b) => 
-            (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
-        ); 
-    }
-    else if (filter === 'HIGH_TO_LOW') {
-        books.sort(
-          (a, b) => 
-            (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
-        ); 
-    }
-    else if (filter === 'RATING') { 
-        books.sort((a, b) => b.rating - a.rating); 
-    }
+  if (filter === "LOW_TO_HIGH") {
+    books.sort(
+      (a, b) =>
+      (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
+    ); 
+  }
+  else if (filter === "HIGH_TO_LOW") {
+    books.sort(
+      (a, b) => 
+      (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
+    ); 
+  }
+  else if (filter === "RATING") {
+    books.sort((a, b) => b.rating - a.rating); 
+  }
 
-    const booksHtml = books
-    .map((book) => {
-        return `<div class="book">
-    <figure class="book__img--wrapper">
-        <img src="${book.url}" alt="" class="book__img" />
-    </figure>
-    <div class="book__title">
-    ${book.title}
-    </div>
-    <div class="book__ratings">
-        ${ratingsHTML(book.rating)}
-    </div>
-    <div class="book__price">
-        ${priceHTML(book.originalPrice, book.salePrice)}
-    </div>
-    </div>`; 
-    })
-    .join(""); 
+  const booksHtml =  books
+  .map((book) => {
+    return `<div class="book">
+                <figure class="book__img--wrapper">
+                  <img class="book__img" src="${book.url}" alt="">
+                </figure>
+                <div class="book__title">
+                  ${book.title}
+                </div>
+                <div class="book__ratings">
+                  ${ratingsHTML(book.rating)}
+                </div>
+                <div class="book__price">
+                  ${riceHTML(book.originalPrice, book.salePrice)}
+                </div>
+              </div>`
+  })
+  .join(""); 
 
-    booksWrapper.innerHTML = booksHtml; 
+  booksWrapper.innerHTML = booksHtml; 
 }
 
 function priceHTML(originalPrice, salePrice) {
-    if (!salePrice) {
-        return `$${originalPrice.toFixed(2)}`; 
-    }
-    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span>$${salePrice.toFixed(2)}`;    
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`;
+  }
+  return `<span class="books__price--normal">$${originalPrice.toFixed(2)}
+  </span>$${salePrice.toFixed(2)}`; 
 }
 
 function ratingsHTML(rating) {
-    let ratingHTML = " "; 
-    for (let i = 0; i < Math.floor(rating); ++i) {
-        ratingHTML += '<i class="fa-solid fa-star"></i>\n'; 
-    }
-    if (!Number.isInteger(rating)) {
-        ratingHTML += '<i class="fa-solid fa-star-half-alt"></i>\n'; 
-    }
-    return ratingHTML; 
+  let ratingHTML = ""; 
+  for(let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fa-solid fa-star"></i>\n'; 
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fa-solid fa-star-half-alt"></i>\n';  
+  }
+  return ratingHTML; 
 }
-
 function filterBooks(event) {
-    renderBooks(event.target.value); 
+  renderBooks(event.target.value); 
 }
 
 setTimeout(() => {
-    renderBooks(); 
-})
+  renderBooks(); 
+}); 
 
-//FAKE DATA
+// FAKE DATA
 function getBooks() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-        resolve([
-    {
+      resolve([
+        {
       id: 1,
-      title: "Crack the coding Interview",
+      title: "Crack the Coding Interview",
       url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
@@ -113,7 +114,7 @@ function getBooks() {
     },
     {
       id: 5,
-      title: "Be obsessed Or Be Average",
+      title: "Be Obsessed Or Be Average",
       url: "assets/book-2.jpeg",
       originalPrice: 32,
       salePrice: 17,
@@ -167,7 +168,7 @@ function getBooks() {
       salePrice: null,
       rating: 4.5,
     },
-  ]);
-}, 1000); 
-}); 
+      ]); 
+    }, 1000); 
+  }); 
 }
